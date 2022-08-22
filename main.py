abce6935e84a64ca8579493b61037f754af7f72f
Nikole Hunter <80729663+octane47@users.noreply.github.com>
2022-08-22 01:36:32 +0000

    began writing the code for the caser cipher

---
+++ main.py
@@ -0,0 +1,28 @@
+import string
+
+alphabets = string.ascii_lowercase + string.ascii_lowercase
+
+sentence = list (input("enter your text : \n").lower())
+
+what_to_do = input("enter encrypt to encrypt, decrypt to cecrypt, exit to exit the program \n'").lower()
+
+shift_number = int(input('enter you sift number from 1 to 25: \n'))
+
+end_program = False
+
+
+while not end_program:
+  #search through enter text
+  if what_to_do == 'encrypt':
+      for i in range(len(sentence)):
+          #get the position of each character within the sentence
+          if sentence[i] =='':
+              sentence[i] = ''
+          else:
+              new_letter = alphabets.index(sentence[i]) + shift_number
+              sentence[i] = alphabets[new_letter]
+      #convert the list back to a string
+      print('',join(map(str,sentence)))
+      end_program = True
+  elif what_to_do == 'decrypt':
+    
