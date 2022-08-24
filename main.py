1bf41cc853bb7dc8947a7d6eb37d0767c0059273
Nikole Hunter <80729663+octane47@users.noreply.github.com>
2022-08-24 22:18:47 +0000

    nearly finished the decrypting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import string
 
-
 alphabets = string.ascii_lowercase + string.ascii_lowercase
 
 sentence = list (input("enter your text : \n").lower())
@@ -23,8 +22,16 @@ while not end_program:
               new_letter = alphabets.index(sentence[i]) + shift_number
               sentence[i] = alphabets[new_letter]
       #convert the list back to a string
-      
-      print('', join(map(str ,sentence)))
+      print('',join(map(str,sentence)))
       end_program = True
-   
-    
+  elif what_to_do == 'decrypt':
+      for i in range(len(sentence)):
+          #get the position of each character within the sentence
+          if sentence[i] =='':
+              sentence[i] = ''
+          else:
+              new_letter = alphabets.index(sentence[i]) + shift_number
+              sentence[i] = alphabets[new_letter]
+      #convert the list back to a string
+      print('',join(map(str,sentence)))
+      end_program = True
