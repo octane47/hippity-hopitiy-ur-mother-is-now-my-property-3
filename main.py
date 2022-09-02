632201dc5243a98ae868b5ada366a01e875d6148
Nikole Hunter <80729663+octane47@users.noreply.github.com>
2022-09-02 00:02:56 +0000

    small update

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
 import string
 #hey just change the e to something to that can mean encyption and paste then change it to something that can mean decyption and then woah wow you did it 
-def en_alphabet_de_alphabet(question):
-    vaild = False
-    while not vaild:
-        response = input(question).lower()
-
-        if response == "en_alphabet" or response == "encrypt":
-            response = "yes"
-            return response
-
-        if response == "no" or response == "n":
-            response = "no"
-            return response
-
-        else:
-            print("please answer yes / no")
-
-plain_text = "hello world"
-shift = 3
+print(" this is your friendly encryption and decryption program")
+print()
+answer = input("what would you like to do?, encrypt or decrypt?")
+print()
 
 
-en_alphabet = string.ascii_lowercase
-shifted = en_alphabet[shift:] + en_alphabet[:shift]
-table = str.maketrans(en_alphabet, shifted)
+if answer = encrypt:
+  en_reply = input(" enter word or phrase you with to encrypt")
+  shift = 3
+  en_alphabet = string.ascii_lowercase
+  shifted = en_alphabet[shift:] + en_alphabet[:shift]
+  table = str.maketrans(en_alphabet, shifted)
+  
+  encrypted = en_reply.translate(table)
+  print()
+  print()
+  print(" )
 
-encrypted = plain_text.translate(table)
 
-print(encrypted)
-import string
 
-plain_text = "hello world"
 shift = -3
 
 
